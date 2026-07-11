33770c2ba0723c5607c019f02b6462f8c293517f
MowlandCodes <mowlandcodes@aol.com>
2026-07-11 22:38:39 +0700

    fix: excessive curly braces on __ADAPTER_IMPORT__ in prisma lib

--- templates/js/server/src/lib/prisma.js
+++ templates/js/server/src/lib/prisma.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@db/client";
-import { __ADAPTER_IMPORT__ } from "__ADAPTER_PACKAGE__";
+import __ADAPTER_IMPORT__ from "__ADAPTER_PACKAGE__";
 
 const adapter = __ADAPTER_INIT__;
 export const prisma = new PrismaClient({ adapter });
